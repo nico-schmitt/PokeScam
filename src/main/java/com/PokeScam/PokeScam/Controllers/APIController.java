b92d28f1aea1632544781ad2d89f6b123e61fd89
Monteasel <53841510+Monteasel@users.noreply.github.com>
2025-12-08 16:30:42 +0100

    can catch wild pokemon

--- src/main/java/com/PokeScam/PokeScam/Controllers/APIController.java
+++ src/main/java/com/PokeScam/PokeScam/Controllers/APIController.java
@@ -1,15 +1,27 @@
 package com.PokeScam.PokeScam.Controllers;
 
+import java.time.Duration;
+import java.util.concurrent.ThreadLocalRandom;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.PokeScam.PokeScam.Model.Pokemon;
 import com.PokeScam.PokeScam.Services.PokemonDataService;
 
+import reactor.core.publisher.Mono;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
 
 @Controller
+@RequestMapping("/api")
 public class APIController {
 
     private final PokemonDataService pkmnDataService;
@@ -18,10 +30,23 @@ public class APIController {
         this.pkmnDataService = pkmnData;
     }
 
-    @PostMapping("/api/addPokemon")
+    @PostMapping("/addPokemon")
     public String addPokemon(@ModelAttribute Pokemon pokemonToAdd, RedirectAttributes redirectAttributes) {
         String addMsg = pkmnDataService.addPokemon(pokemonToAdd);
         redirectAttributes.addFlashAttribute("addMsg", addMsg);
         return "redirect:/";
+    }
+
+    @GetMapping("/tryCatchPkmn")
+    @ResponseBody
+    public String isCatchSuccessful(@RequestParam("name") String pkmnToCatchName) {
+        final long catchDelay = ThreadLocalRandom.current().nextLong(100, 1500);
+        boolean isSuccessful = ThreadLocalRandom.current().nextFloat() < 0.5 ? true : false; 
+        if(isSuccessful) {
+            Pokemon pkmnToAdd = new Pokemon();
+            pkmnToAdd.setName(pkmnToCatchName);
+            pkmnDataService.addPokemon(pkmnToAdd);
+        }
+        return Mono.delay(Duration.ofMillis(catchDelay)).thenReturn(Boolean.toString(isSuccessful)).block();
     }
 }
